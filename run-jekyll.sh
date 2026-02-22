46141b2629963fdd198fe3f10263a5174ac6b1b2
Steffen Mueller <steffenmueller4@users.noreply.github.com>
2026-02-22 11:15:05 +0100

    Update run-jekyll.sh

--- run-jekyll.sh
+++ run-jekyll.sh
@@ -5,4 +5,4 @@ CURRENT_DIRECTORY=$(pwd)
 
 # Jekyll requires a rootful podman machine: podman machine set --rootful
 #podman machine set --rootful
-podman run -it -p 4000:4000 -v $(pwd):/site bretfisher/jekyll-serve
+docker run -it -p 4000:4000 -v $(pwd):/site bretfisher/jekyll-serve

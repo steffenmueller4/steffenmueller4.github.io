3b89249c1a66ef6105647d06e6d856c21f2b350f
Steffen Mueller <steffenmueller4@users.noreply.github.com>
2026-04-06 16:25:32 +0200

    Changes the setup to run the blog locally.

--- run-jekyll.bat
+++ run-jekyll.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set JEKYLL_VERSION=4.2.0
 set CURRENT_DIRECTORY="%cd%"
 
-podman run --rm -v "%CURRENT_DIRECTORY%:/srv/jekyll" -p 4000:4000 -it "jekyll/jekyll:%JEKYLL_VERSION%" /bin/bash -c "bundle install && bundle exec jekyll serve --host 0.0.0.0"
+docker build -t jekyll-blog .
+docker run --rm -v "%CURRENT_DIRECTORY%:/site" -p 4000:4000 -p 35729:35729 jekyll-blog
--- run-jekyll.sh
+++ run-jekyll.sh
@@ -3,6 +3,5 @@ set +eux
 
 CURRENT_DIRECTORY=$(pwd)
 
-# Jekyll requires a rootful podman machine: podman machine set --rootful
-#podman machine set --rootful
-podman run -it -p 4000:4000 -v $(pwd):/site bretfisher/jekyll-serve
+docker build -t jekyll-blog .
+docker run --rm -v "$(pwd):/site" -p 4000:4000 -p 35729:35729 jekyll-blog
